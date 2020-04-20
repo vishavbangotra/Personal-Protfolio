a8da72c1e90127d3d7228af5e3469577ced5c1bf
Vishav Bangotra <44341523+vishavbangotra@users.noreply.github.com>
2020-04-20 19:29:57 +0530

    Added jquery and javascript to create scroll interfacing on arrow down button

---
+++ CV_Project/static/script/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+$(".arrow-down").click(function() {
+     $('html, body').animate({
+         scrollTop: $(".service-headline").offset().top
+     }, 1000);
+ });
+});
