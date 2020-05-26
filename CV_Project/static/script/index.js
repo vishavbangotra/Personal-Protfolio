9effbfad725bc7f27821a0a718d7db8b17fe676d
Vishav Bangotra <44341523+vishavbangotra@users.noreply.github.com>
2020-05-26 11:05:26 +0530

    added javascript and responsiveness

--- CV_Project/static/script/index.js
+++ CV_Project/static/script/index.js
@@ -6,3 +6,37 @@ $(".arrow-down").click(function() {
  });
 });
 
+var subheadline = [
+    'A',' ','C', 'r','e',
+    'a','t','i','v','e',' ', 'p',
+    'r','o','g','r','a','m','m','e','r'
+]
+var index = 0;
+setInterval(function(){
+        $('#aboutmesubheadline').append(subheadline[index++]);
+}, 100)
+
+var windowSize = $(window).width();
+
+if (windowSize < 500) {
+    $('.logo-bar').hide();
+}
+else{
+    $('.logo-bar').show();
+}
+
+var bangotra = ['B','a','n','g','o','t','r','a']
+var  i=0;
+setInterval(function(){
+    $('.last-name').append(bangotra[i]);
+    // $('.webd').append(fullstackdeveloper[i]);
+    i++;
+}, 150)
+
+var fullstackdeveloper = ['F','u','l','l','-',
+                        's','t','a','c','k',
+                        'D','e','v','e','l','o','p','e','r']
+// var  i=0;
+// setInterval(function(){
+//     $('.last-name').append(bangotra[i++]);
+// }, 200)
